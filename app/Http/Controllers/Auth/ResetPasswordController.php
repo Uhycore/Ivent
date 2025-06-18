b76637bed3e31c429b2335c6b78255e74a943c27
Habi <evilunicornz368@gmail.com>
2025-06-18 13:39:52 +0700

    ayo rill

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -21,24 +22,55 @@ class ResetPasswordController extends Controller
 
     public function reset(Request $request)
     {
-        $request->validate([
-            'token'    => 'required',
-            'email'    => 'required|email',
-            'password' => 'required|min:8|confirmed',
-        ]);
-
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user, $password) {
-                $user->forceFill([
-                    'password' => Hash::make($password),
-                    'remember_token' => Str::random(60),
-                ])->save();
-            }
-        );
-
-        return $status == Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
-            : back()->withErrors(['email' => [__($status)]]);
+    try {
+        $email = Crypt::decryptString($request->input('email'));
+    } catch (\Exception $e) {
+        return back()->withErrors(['email' => 'Email tidak valid atau rusak']);
     }
+
+    $request->merge(['email' => $email]);
+
+    $request->validate([
+        'token'    => 'required',
+        'email'    => 'required|email',
+        'password' => 'required|min:8|confirmed',
+    ]);
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user, $password) {
+            $user->forceFill([
+                'password' => Hash::make($password),
+                'remember_token' => Str::random(60),
+            ])->save();
+        }
+    );
+
+    return $status == Password::PASSWORD_RESET
+        ? redirect()->route('login')->with('status', __($status))
+        : back()->withErrors(['email' => [__($status)]]);
+ }
+
+    //public function reset(Request $request)
+    // {
+    //     $request->validate([
+    //         'token'    => 'required',
+    //         'email'    => 'required|email',
+    //         'password' => 'required|min:8|confirmed',
+    //     ]);
+
+    //     $status = Password::reset(
+    //         $request->only('email', 'password', 'password_confirmation', 'token'),
+    //         function ($user, $password) {
+    //             $user->forceFill([
+    //                 'password' => Hash::make($password),
+    //                 'remember_token' => Str::random(60),
+    //             ])->save();
+    //         }
+    //     );
+
+    //     return $status == Password::PASSWORD_RESET
+    //         ? redirect()->route('login')->with('status', __($status))
+    //         : back()->withErrors(['email' => [__($status)]]);
+    // }
 }
